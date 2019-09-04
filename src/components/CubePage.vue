2a3f82af53603adeaa824aee533e48b0bcd546c0
Taras Vitovshchyk <vitovstreet@gmail.com>
2019-09-04 09:49:06 +0300

    added sphere with ukrainiancomments

--- src/components/CubePage.vue
+++ src/components/CubePage.vue
@@ -1,6 +1,5 @@
 <template>
-	<div class="container">
-		<h1>Testing 3D cube!</h1>
+	<div class="container-box">
 		<div id="background"></div>
 	</div>
 </template>
@@ -8,6 +7,7 @@
 
 <script>
 import * as THREE from 'three'
+import * as OrbitControls from 'three-orbitcontrols'
 import store from '../store'
 
 
@@ -22,30 +22,54 @@ export default {
 		}
 	},
 	mounted() {
-		var scene = new THREE.Scene();
+		var scene = new THREE.Scene();//добавляємо сцену розміщення об'єкту
 		var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+		var renderer = new THREE.WebGLRenderer();//взаємодія з WebGL API
+		renderer.setSize( window.innerWidth, window.innerHeight );//рендеринг розмірів, продукування елементу
 
-		var renderer = new THREE.WebGLRenderer();
-		renderer.setSize( window.innerWidth, window.innerHeight );
-		document.body.appendChild( renderer.domElement );
+		var controls = new THREE.OrbitControls( camera, renderer.domElement );//підключаємо обертання елементу вручну
 
-		var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-		var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-		var cube = new THREE.Mesh( geometry, material );
-		scene.add( cube );
+		document.getElementById("background").appendChild(renderer.domElement);//вставляємо в DOM
 
-		camera.position.z = 5;
+		//camera.position.set( 1, 1, 1 ); ???
+		controls.update();
 
+
+
+		//var geometry = new THREE.BoxGeometry( 1, 2, 1 );//визначення значень по осях x,y,z
+		var geometry = new THREE.SphereGeometry( 2, 20, 20 );//створюємо сферу
+		var material = new THREE.MeshBasicMaterial( { 
+			color: '#433F81',
+			wireframe: true,
+			// widthSegments: 10, 
+			// heightSegments: 10,
+			} );//вказуємо значення для сітки елементу, тобто його стилізація
+
+		var cube = new THREE.Mesh( geometry, material );//поміщаємо значення координат та стилів в сітку, де буде знаходитися наш об'єкт
+		scene.add( cube );//передаємо виконаний елемент в сцену, для відображення
+
+
+		camera.position.z = 10;//віддаляє або приближає об'єкт, свого роду масштабування
+		camera.position.x = 0;//зміщення по осі х
+		camera.position.y = 0;//зміщення по осі y
+
+
+
+
+		//анімуємо об'єкт
 		var animate = function () {
 			requestAnimationFrame( animate );
 
 			cube.rotation.x += 0.01;
 			cube.rotation.y += 0.01;
+			//cube.rotation.z += 0.01;
 
 			renderer.render( scene, camera );
 		};
 
 		animate();
+
+
 	}
 }	
 </script>
